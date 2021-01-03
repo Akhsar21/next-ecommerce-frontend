d00130c3852c5434162c0832139359fd25006faa
Akhsar21 <akhsarodhe@gmail.com>
2021-01-03 13:47:59 +0800

    edit

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,11 +1,11 @@
-import { Footer, Meta } from "components";
+import { Footer } from "components";
 
 import styles from "./Layout.module.css";
 
 export default function Layout({ children }) {
   return (
     <>
-      <Meta />
+      {/* <Meta /> */}
       {children}
       <Footer />
     </>
